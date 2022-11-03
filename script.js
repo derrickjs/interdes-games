052c571a8348579aac1ce9bd0ebf044232802253
derrickjs <85529367+derrickjs@users.noreply.github.com>
2022-11-03 09:56:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function hover(element) {
+    element.setAttribute('src', 'https://res.cloudinary.com/derrickstahlman/image/upload/v1667407118/mod8_gif1_fwy13y.gif');
+  }
+  
+  function unhover(element) {
+    element.setAttribute('src', 'https://res.cloudinary.com/derrickstahlman/image/upload/v1667407130/mod8_img1_awm80y.png');
+  }
+  
+
+  
