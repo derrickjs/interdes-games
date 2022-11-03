301ffcb6a647266fed1438013ae908c28a3b7e22
Kelsey <downsk282@my.clevelandcc.edu>
2022-11-03 10:46:56 -0400

    added content

--- script.js
+++ script.js
@@ -6,5 +6,20 @@ function hover(element) {
     element.setAttribute('src', 'https://res.cloudinary.com/derrickstahlman/image/upload/v1667407130/mod8_img1_awm80y.png');
   }
   
+function addAnimate(){
+  document.querySelector('.headerleft').classList.add("animate__animated");
+  document.querySelector('.headerleft').classList.add("animate__bounce");
+}
+
+document.querySelector('.headerleft').addEventListener("mouseenter", addAnimate)
+
+function removeAnimate(){
+  document.querySelector('.headerleft').classList.remove("animate__animated");
+  document.querySelector('.headerleft').classList.remove("animate__bounce");
+}
+
+document.querySelector('.headerleft').addEventListener("mouseleave", removeAnimate)
+
+
 
   
